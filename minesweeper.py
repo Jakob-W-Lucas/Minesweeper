b91ed4b31b98add118a7c14a381540d639e686e9
t0astie <jakob.w.lucas@gmail.com>
2025-02-02 14:54:04 +1100

    Sprite implementation and documentation

--- minesweeper.py
+++ minesweeper.py
@@ -10,31 +10,58 @@ import random
 
 pygame.init()
 
+# Size of the grid (length * width)
 grid_size = 15
-cell_size = 6
+# Size of each cell
+cell_size = 8
+# Scale size for each game pixel
 pixel_size = 10
-line_size = 2 
+# Length and width for each cell
 cell_lw = cell_size * pixel_size
+# Size of lines separating cells
+line_size = 2 
 
 # Screen size and creation
 SCREEN_WIDTH = (15 * cell_lw) + (grid_size + 1) * line_size
 SCREEN_HEIGHT = SCREEN_WIDTH
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
 # Colors:
-c_white = pygame.Color(213, 224, 216)       # White
-c_grey  = pygame.Color(139, 143, 140)       # Grey
-c_black = pygame.Color(24, 14, 26)          # Black
+CRT_WHITE = (213, 224, 216)       # White
+CRT_GREY  = (139, 143, 140)       # Grey
+CRT_BLACK = (24, 14, 26)          # CRT Black
+BLACK = (0, 0, 0)
+
+# Display the screen
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Minesweeper')
+
+# Load the sprite sheet
+sprite_sheet_image = pygame.image.load('minesweeper_sprites.png').convert_alpha()
+
+# Get the individual images from the sprite sheet
+def get_image(sheet, frame_x, frame_y, width, height, scale, color):
+    
+    # Create a base square to place sprite image on top of
+    image = pygame.Surface((width, height)).convert_alpha()
+    image.blit(sheet, (0, 0), ((frame_x * width), (frame_y * height), width, height))
+    
+    # Transform the image
+    image = pygame.transform.scale(image, (width * scale, height * scale))
+    image.set_colorkey(color)
+    
+    return image
+
+# Images:
+s_CELL = get_image(sprite_sheet_image, 0, 1, 8, 8, pixel_size, BLACK)
+s_MINE = get_image(sprite_sheet_image, 1, 1, 8, 8, pixel_size, BLACK)
 
 # All the current cells in the grid
 cells = []
 
 class Cell(pygame.sprite.Sprite):
-    def __init__(self, col, x, y):
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((cell_lw, cell_lw))
-        self.image.fill(col)
+        self.image = s_CELL
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
         
@@ -42,7 +69,14 @@ class Cell(pygame.sprite.Sprite):
         self.surrounding_mines = 0
         
     def clicked(self):
-        self.image.fill(c_black)
+        
+        if self.mine and self.surrounding_mines == 0:
+            self.image = pygame.Surface((cell_lw, cell_lw))
+            self.image.fill(CRT_BLACK)
+        elif self.mine:
+            self.image = s_MINE
+        else:
+            self.image = get_image(sprite_sheet_image, self.surrounding_mines - 1, 0, 8, 8, pixel_size, BLACK)
     
     def reveal_cell(self):
         pass
@@ -55,8 +89,8 @@ def draw_grid():
         square = i * (cell_lw + line_size)
         
         # Each horizontal and vertical line in the grid
-        pygame.draw.line(screen, c_grey, (0, square), (SCREEN_WIDTH, square), line_size)
-        pygame.draw.line(screen, c_grey, (square, 0), (square, SCREEN_HEIGHT), line_size)
+        pygame.draw.line(screen, CRT_GREY, (0, square), (SCREEN_WIDTH, square), line_size)
+        pygame.draw.line(screen, CRT_GREY, (square, 0), (square, SCREEN_HEIGHT), line_size)
 
 def create_cells():
     # Create each sprite in the grid
@@ -70,7 +104,8 @@ def create_cells():
             
             x = cell_lw * (j + 0.5) + line_size * (j + 1)
             # Create the cell and add it to the row
-            cell = Cell(c_white, x, y)
+            cell = Cell(x, y)
+            cell.surrounding_mines = random.randint(0, 8)
             row.add(cell)
             
         cells.append(row)
@@ -91,10 +126,10 @@ while run:
     if len(cells) == 0:
         create_cells()
         
-    # Create the screen and get the key presses
-    screen.fill(c_black)
-    key = pygame.key.get_pressed()
+    # Create the screen background
+    screen.fill(CRT_BLACK)
     
+    # Display Grid
     draw_grid()
     
     for row in cells:
@@ -102,7 +137,10 @@ while run:
         row.update()
         # Draw each cell sprite in the grid
         row.draw(screen)
-            
+    
+    # Get key presses
+    key = pygame.key.get_pressed()
+    
     # Exit
     if key[pygame.K_ESCAPE]:
        run = False
