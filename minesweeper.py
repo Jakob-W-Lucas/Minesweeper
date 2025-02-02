80e18c9bfe608c142386d282283cf92bc7f38362
t0astie <jakob.w.lucas@gmail.com>
2025-02-02 13:47:37 +1100

    Sprite groups for each cell to indicate clicks

--- minesweeper.py
+++ minesweeper.py
@@ -22,36 +22,67 @@ SCREEN_HEIGHT = SCREEN_WIDTH
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-class Cell():
-    def __init__(self):
+# Colors:
+c_white = pygame.Color(213, 224, 216)       # White
+c_grey  = pygame.Color(139, 143, 140)       # Grey
+c_black = pygame.Color(24, 14, 26)          # Black
+
+# All the current cells in the grid
+cells = []
+
+class Cell(pygame.sprite.Sprite):
+    def __init__(self, col, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((cell_lw, cell_lw))
+        self.image.fill(col)
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        
         self.mine = False
         self.surrounding_mines = 0
+        
+    def clicked(self):
+        self.image.fill(c_black)
     
     def reveal_cell(self):
         pass
 
 # Draws the minesweeper grid
 def draw_grid():
+    # Draw each line in the grid
     for i in range(grid_size + 1):
-        pygame.draw.line(screen, pygame.Color(213, 224, 216), (0, i * (cell_lw + line_size)), (SCREEN_WIDTH, i * (cell_lw + line_size)), line_size)
-        pygame.draw.line(screen, pygame.Color(213, 224, 216), (i * (cell_lw + line_size), 0), (i * (cell_lw + line_size), SCREEN_HEIGHT), line_size)
+        # Size encompassing 1 cell 
+        square = i * (cell_lw + line_size)
+        
+        # Each horizontal and vertical line in the grid
+        pygame.draw.line(screen, c_grey, (0, square), (SCREEN_WIDTH, square), line_size)
+        pygame.draw.line(screen, c_grey, (square, 0), (square, SCREEN_HEIGHT), line_size)
+
+def create_cells():
+    # Create each sprite in the grid
+    for i in range(grid_size):
+        
+        row = pygame.sprite.Group()
+        
+        y = cell_lw * (i + 0.5) + line_size * (i + 1)
+        
+        for j in range(grid_size):
+            
+            x = cell_lw * (j + 0.5) + line_size * (j + 1)
+            # Create the cell and add it to the row
+            cell = Cell(c_white, x, y)
+            row.add(cell)
+            
+        cells.append(row)
 
 # Returns the cell based on coords from the screen
 def get_cell(x: int, y: int) -> tuple[int, int]:
     
+    # Calculate which cell has been clicked
     cell_x = x // (cell_lw + line_size)
     cell_y = y // (cell_lw + line_size)
     
     return [cell_x, cell_y]
-
-cells = []
-
-def create_cells():
-    for _ in range(grid_size):
-        row = []
-        for _ in range(grid_size):
-            row.append(Cell())
-        cells.append(row)
 
 run = True
 # Game loop
@@ -61,21 +92,28 @@ while run:
         create_cells()
         
     # Create the screen and get the key presses
-    screen.fill((24, 14, 26))
+    screen.fill(c_black)
     key = pygame.key.get_pressed()
     
     draw_grid()
     
+    for row in cells:
+        # Update each cell in the grid
+        row.update()
+        # Draw each cell sprite in the grid
+        row.draw(screen)
+            
     # Exit
     if key[pygame.K_ESCAPE]:
        run = False
        
     # Quit game
     for event in pygame.event.get():
-        
-        if event.type == pygame.MOUSEBUTTONUP:
+        # Call which cell was clicked
+        if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            print(get_cell(pos[0], pos[1]))
+            r, c = get_cell(pos[0], pos[1])
+            cells[c].sprites()[r].clicked()
       
         if event.type == pygame.QUIT:
             run = False
